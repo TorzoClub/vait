aed98a8c8b1c058ab9a269590a5f6664300e286f
vec <vec@torzo.club>
2022-05-18 23:40:16 +0800

    [add] Atomic

---
+++ src/atomic.test.ts
@@ -0,0 +1,88 @@
+import { Atomic } from './'
+import { timeout } from './timeout'
+
+test('Atomic ignore error', async () => {
+  const atomic = Atomic()
+
+  const history: number[] = []
+  const waiting: Promise<unknown>[] = []
+
+  async function createFailure() {
+    throw new Error('failure')
+  }
+
+  const failure = createFailure()
+
+  await expect(failure).rejects.toThrow()
+
+  waiting.push(
+    atomic(() => failure).catch(() => {})
+  )
+
+  waiting.push(
+    atomic(async () => {
+      history.push(2)
+    })
+  )
+  
+  await Promise.all(waiting)
+  
+  expect(history).toEqual([2])
+})
+
+test.only('Atomic sync', async () => {
+  const atomic = Atomic()
+
+  const count = 1000
+  const waitting: Promise<number>[] = []
+  for (let c = 0; c < count; ++c) {
+    waitting.push(
+      atomic(async () => c)
+    )
+  }
+
+  const list = await Promise.all(waitting)
+  for (let c = 0; c < count; ++c) {
+    expect(c).toEqual(list[c])
+  }
+})
+
+test('Atomic timeout', async () => {
+  const history: number[] = []
+  const waiting: Promise<unknown>[] = []
+
+  const atomic = Atomic()
+  waiting.push(
+    atomic(async () => {
+      await timeout(1000)
+      history.push(1)
+    })
+  )
+  waiting.push(
+    atomic(async () => {
+      await timeout(3000)
+      history.push(2)
+    })
+  )
+  waiting.push(
+    atomic(async () => {
+      await timeout(10)
+      history.push(3)
+    })
+  )
+  waiting.push(
+    atomic(async () => {
+      history.push(4)
+    })
+  )
+  waiting.push(
+    atomic(async () => {
+      await timeout(100)
+      history.push(5)
+    })
+  )
+  
+  await Promise.all(waiting)
+  
+  expect(history).toEqual([1, 2, 3, 4, 5])
+})
