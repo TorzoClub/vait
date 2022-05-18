1fb041eb746da90b151731690a2f84069031b073
vec <vec@torzo.club>
2022-05-18 23:57:43 +0800

    [add] timeout

---
+++ src/timeout.test.ts
@@ -0,0 +1,22 @@
+import { timeout } from "./timeout"
+
+test('timeout', async () => {
+  let value: number | undefined = undefined
+
+  const promise = timeout(100).then(() => {
+    value = 100
+  })
+  expect(value).toBe(undefined)
+
+  await promise
+
+  expect(value).toBe(100)
+})
+
+test('timeout timing', async () => {
+  const start_time = Date.now()
+  await timeout(1000)
+  const end_time = Date.now()
+  const time_difference = end_time - start_time
+  expect(time_difference).toBeGreaterThanOrEqual(1000)
+})
---
+++ src/timeout.ts
@@ -0,0 +1,1 @@
+export const timeout = (ms: number) => new Promise(res => setTimeout(res, ms))
