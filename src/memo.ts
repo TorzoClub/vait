b6dd9f4a95d49eb64a04846809d81e81c696f85a
vec <vec@torzo.club>
2022-06-20 13:10:16 +0800

    [improve] Wrap reuse Memo's Getter

--- src/memo.ts
+++ src/memo.ts
@@ -1,8 +1,16 @@
-export type Memo<D> = Readonly<[() => D, (new_data: D) => void]>
+interface Getter<D> {
+  (): D
+}
+
+interface Setter<D> {
+  (new_data: D): void
+}
+
+export type Memo<D> = Readonly<[Getter<D>, Setter<D>]>
 
 export const Memo = <D>(data: D): Memo<D> => [
   () => data,
   (new_data: D) => { data = new_data }
 ]
 
-export const Wrap = <D>(data: D) => () => data
+export const Wrap = <D>(data: D) => Memo(data)[0]
