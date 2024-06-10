29850d172dd9081369179a9442cf5e2e1cdd5662
vec <vec@torzo.club>
2024-06-10 09:46:52 +0800

    [add] rejectTimeout

--- src/timeout.test.ts
+++ src/timeout.test.ts
@@ -1,4 +1,4 @@
-import { timeout } from './timeout'
+import { TimeoutRejected, rejectTimeout, timeout } from './timeout'
 
 test('timeout', async () => {
   let value: number | undefined = undefined
@@ -19,4 +19,24 @@ test('timeout timing', async () => {
   const end_time = Date.now()
   const time_difference = end_time - start_time
   expect(time_difference).toBeGreaterThanOrEqual(1000)
+})
+
+test('rejectTimeout', async () => {
+  const waiting = timeout(100)
+  rejectTimeout(waiting)
+  expect(waiting).rejects.toThrow(TimeoutRejected)
+})
+
+test('rejectTimeout 无效的waiting', async () => {
+  const waiting = timeout(100)
+  rejectTimeout(new Promise(() => {}))
+
+  let c = 0
+  waiting.then(() => {
+    c += 1
+  })
+
+  await timeout(150)
+
+  expect(c).toBe(1)
 })
--- src/timeout.ts
+++ src/timeout.ts
@@ -1,7 +1,28 @@
-import { Wait } from './wait'
+import { OutterPromise } from './outter-promise'
+import { Timer } from './timer'
 
-export async function timeout(ms: number) {
-  const [ wait, go ] = Wait()
-  setTimeout(go, ms)
-  return wait
+type RejectTiemout = (reason?: unknown) => void
+
+type TimeoutWaiting = Promise<void>
+
+const handlers = new WeakMap<TimeoutWaiting, RejectTiemout>()
+
+export function rejectTimeout(waiting: TimeoutWaiting) {
+  const cancel = handlers.get(waiting)
+  if (cancel) {
+    handlers.delete(waiting)
+    cancel()
+  }
+}
+
+export class TimeoutRejected extends Error {}
+
+export function timeout(ms: number) {
+  const [ resolve, reject, waiting ] = OutterPromise<void>()
+  const cancelTimer = Timer(ms, resolve)
+  handlers.set(waiting, (cause) => {
+    cancelTimer()
+    reject(new TimeoutRejected('timeout cancel', { cause }))
+  })
+  return waiting
 }
