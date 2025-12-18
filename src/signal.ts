292b9e2676c11c1cc925c75f281a32f2b803298d
vec <vec@torzo.club>
2025-12-18 21:37:17 +0800

    [fix] fix Signal Generics & improving Handlers execution

--- src/signal.ts
+++ src/signal.ts
@@ -11,53 +11,51 @@ export class SignalError extends Error {}
 // 进而会影响调用方的代码执行，这是不太好的
 // 故 executeHandlers 将会忽略错误，并将跳过当前出错的函数
 // 继续执行队列中余下的函数
-export function executeHandlers<P>(handlers: Handlers<P>, payload: P) {
-  for (let i = 0; i < handlers.length; ++i) {
-    try {
-      handlers[i](payload)
-    } catch (err) {
-      console.warn(
-        'Signal has caught an error, but it is ignored by default and not thrown. ' +
-        'If you want to throw this error, please use triggerCareError(). Error details:', err
-      )
-    }
-  }
-}
-
-function executeHandlersCareError<P>(handlers: Handlers<P>, payload: P) {
-  try {
+export const ExecuteHandlers = <P>(ignore_error: boolean, getHandlers: () => Handler<P>[]) => (
+  (payload: P) => {
+    const handlers = getHandlers()
     for (let i = 0; i < handlers.length; ++i) {
-      handlers[i](payload)
+      try { handlers[i](payload) } catch (cause) {
+        if (ignore_error) {
+          console.warn(
+            'Signal has caught an error, but it is ignored by default and not thrown. ' +
+            'If you want to throw this error, please use triggerCareError(). Error details:', cause
+          )
+        } else {
+          throw new SignalError('An error occurred while executing the function queue registered in the signal.', { cause })
+        }
+      }
     }
-  } catch (cause) {
-    throw new SignalError('An error occurred while executing the function queue registered in the signal.', { cause })
   }
-}
+)
 
-export interface Signal<P = void> {
+export type Signal<P = void> = Readonly<{
+  trigger: P extends void ? () => void : (payload: P) => void
+  triggerCareError: P extends void ? () => void : (payload: P) => void
+  receive(fn: (payload: P) => void): () => void
+  cancelReceive(fn: (payload: P) => void): void
   isEmpty(): boolean
-  trigger(payload: P): void
-  triggerCareError(payload: P): void
-  receive(fn: Handler<P>): () => void
-  cancelReceive(fn: Handler<P>): void
-}
+}>
+
 export function Signal(): Signal
 export function Signal<P>(): Signal<P>
 export function Signal<P>(): Signal<P> {
   const [getHandlers, setHandlers] = Memo<Handlers<P>>([])
-  const cancelReceive = (fn: Handler<P>) => {
+
+  const cancelReceive = (fn: Handler<P>) => (
     setHandlers( removeByItem(getHandlers(), fn) )
-  }
+  )
+
   return {
-    isEmpty: () => (getHandlers().length === 0),
-    triggerCareError: payload => executeHandlersCareError(getHandlers(), payload),
-    trigger: payload => executeHandlers(getHandlers(), payload),
+    isEmpty: () => getHandlers().length === 0,
+    trigger: ExecuteHandlers(true, getHandlers) as Signal<P>['trigger'],
+    triggerCareError: ExecuteHandlers(false, getHandlers) as Signal<P>['triggerCareError'],
     cancelReceive,
     receive(fn) {
       setHandlers(getHandlers().concat(fn))
       return () => cancelReceive(fn)
     },
-  } as const
+  }
 }
 
 Signal.once = <P>(sig: Signal<P>, fn: Handler<P>) => {
